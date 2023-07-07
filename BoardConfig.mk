ad82fb176e6286347406ae659b42fd3911397f38
boedhack99 <boedhack@gmail.com>
2023-07-07 15:58:36 +0700

    sunstone: Import prebuilt kernel|dtb|dtb|modules from stock * from sunstone MIUI V14.0.2.0.SMQMIXM release-keys
    Change-Id: I6ef27f6a0e336374147938a5eff7af98bfe5b393

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,8 +45,6 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_DTB_OFFSET := 0x01f00000
-BOARD_DTB_SIZE := 979076
-BOARD_HEADER_SIZE := 2112
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -81,6 +79,20 @@ BOARD_KERNEL_CMDLINE += \
     service_locator.enable=1 \
     swiotlb=0 \
     video=vfb:640x400,bpp=32,memsize=3072000
+
+# Kernel prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := vendor/holi_GKI.config
+TARGET_KERNEL_HEADERS := kernel/xiaomi/sm6375
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6375
+
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/prebuilt/ramdisk-modules/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)
 
 # Metadata
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
