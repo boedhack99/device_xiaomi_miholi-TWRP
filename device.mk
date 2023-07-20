fa516fa092e36e08972dabd291c3a9a73f1d02ce
boedhack99 <boedhack@gmail.com>
2023-07-20 18:17:03 +0700

    miholi: Include 64bit only
    Change-Id: Iccdf2a8102bef07a01a6ab7cd5b9dcc93884c339

--- device.mk
+++ device.mk
@@ -5,8 +5,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
