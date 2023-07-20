9a1ac7af0bf70300db38a8bc94c5eaa77ef948e9
boedhack99 <boedhack@gmail.com>
2023-07-20 18:49:38 +0700

    miholi: Build back libdisplayconfig.qti
    Change-Id: Iaf8d8ff5511d40a2fd525899e11744446005a16f

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
+    libdisplayconfig.qti \
     libqtivibratoreffect \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
@@ -73,6 +74,7 @@ RECOVERY_BINARY_SOURCE_FILES += \
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.qti.hardware.vibrator.impl.so \
